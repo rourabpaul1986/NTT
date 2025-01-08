688b3b92a6abe062ee54c8b01243afc000c773d9
rourabpaul1986 <62745577+rourabpaul1986@users.noreply.github.com>
2025-01-08 14:12:04 +0530

    Update berrett_top.vhd

--- barrett/berrett_top.vhd
+++ barrett/berrett_top.vhd
@@ -22,7 +22,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
-
+use ieee.math_real.all;
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
@@ -34,8 +34,8 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity barrett_top is
     generic (
-        L : integer := 16;        -- Length of the operands
-        w : integer := 8;         -- Width of the segments
+        L : integer := 20;        -- Length of the operands
+        w : integer := 4;         -- Width of the segments
         --mu : integer := 59127;         -- # Equivalent to 2^k // N
         N : integer := 72639    -- The modulus    
     );
@@ -53,33 +53,35 @@ end barrett_top;
 
 architecture Behavioral of barrett_top is
    -- State definitions
-    type state_type is (IDLE, C_COM, Q_COM, T_COM, R2_COM, R3_com, i_CHK, FINALIZE);
+    type state_type is (IDLE, C_COM, CxMU_COM, QxN_COM, AddR_COM, T_COM, i_CHK, FINALIZE);
     signal state : state_type;
 
     -- Constants
 
     constant k : integer := 2*l; --32 for w=4
     constant mu : integer := (2**(2*l)) / n; --2^k//n
-    --constant s : integer := 2*w+((w-1+w-1)*w); --32 for w=4
     constant s : integer := 2*w+((l/w-1+l/w-1)*w); --32 for w=4
-    
+    constant logN : positive := positive(ceil(log2(real(N))));
 
     -- Internal signals
-    signal T_reg, A_reg, B_reg : std_logic_vector(l-1 downto 0) := (others => '0');
-    signal C_reg : std_logic_vector(2*w-1 downto 0) := (others => '0');
-    signal C_shift : std_logic_vector(s downto 0) := (others => '0');
-    --signal zero_pad : std_logic_vector(w*(w-1+w-1) downto 0):=(others => '0');
-    signal zero_pad : std_logic_vector(w*(l/w-1+l/w-1) downto 0):=(others => '0');
-    signal Q : std_logic_vector(2*s-1 downto 0):=(others => '0');
-    signal R1, R2, result : std_logic_vector(s-1 downto 0):=(others => '0');
-    signal zero_qpad : std_logic_vector(k-1 downto 0):=(others => '0');      
-    signal i, j : integer := 0;  -- To keep track of the loop index
+    
+    signal  A_reg, B_reg : std_logic_vector(L-1 downto 0) := (others => '0'); -- No change
+    signal C_reg : std_logic_vector(2*w-1 downto 0) := (others => '0'); -- No change, optimal for w-bit multiplication
+    signal C_shift : std_logic_vector((2*w + ((l/w-1 + l/w-1)*w))-1 downto 0) := (others => '0'); -- Optimized bit-width
+    signal zero_pad : std_logic_vector(w*(l/w-1 + l/w-1)-1 downto 0) := (others => '0'); -- Optimized
+    signal Q : std_logic_vector((2*w + ((l/w-1 + l/w-1)*w) + k)-1 downto 0) := (others => '0'); -- Adjusted based on required width
+    signal R1, R2, T_reg : std_logic_vector(L-1 downto 0) := (others => '0'); -- Adjusted to match modular reduction width
+    signal zero_qpad : std_logic_vector(k-1 downto 0) := (others => '0'); -- No change
+    signal i, j : natural range 0 to l/w-1 := 0; -- Optimized for loop index
+
     signal fault_reg : std_logic:= '0';
     
     
 begin
 
-    
+    assert logN < L
+    report "Error:The number of bit required to store N is greater than or equal to L. Please adjust the parameters."
+    severity FAILURE; 
 
     -- Next state logic
     process(state, start, clk, reset)     
@@ -90,7 +92,7 @@ begin
     
          if reset = '1' then
             state <= IDLE;
-            result <= (others => '0');
+            T_reg <= (others => '0');
             A_reg <= (others => '0');
             i <= 0;
             j <= 0;
@@ -116,44 +118,33 @@ begin
                  else
                     C_shift(2*w+(i+j)*w-1 downto 0)<=std_logic_vector(to_unsigned((to_integer(unsigned(A_reg(w-1+i*w downto 0+i*w))) * to_integer(unsigned( B_reg(w-1+j*w downto 0+j*w)))), C_reg'length)) & zero_pad((i+j)*w-1 downto 0);
                   end if;
-                  state <= Q_COM;
+                  state <= CxMU_COM;
                     
                     
-              when Q_COM=>                            
-                 --Q <= zero_qpad & std_logic_vector(to_unsigned(to_integer(unsigned(C_shift)) * mu, Q'length)(2*s-1 downto k));
-                 --Q <= std_logic_vector(to_unsigned(to_integer(unsigned(C_shift)) * mu, Q'length));
-                -- Q <= std_logic_vector(resize(unsigned(C_shift) * to_unsigned(mu, C_shift'length), Q'length)); --c_shift*mu
-                 Q <= std_logic_vector(resize(unsigned(C_shift) * to_unsigned(mu, C_shift'length), Q'length)); --c_shift*mu
-                
-                 state <= T_COM;
+             when CxMU_COM=>                            
+                 Q <= std_logic_vector(resize(unsigned(C_shift) * to_unsigned(mu, C_shift'length), Q'length)); --c_shift*mu                
+                 state <= QxN_COM;
                   
-             when T_COM=>  
-                 --R<=Q(2*s-1 downto s);
-                 R1 <= std_logic_vector(resize(unsigned(Q(2*s-1 downto s)) * to_unsigned(N, R1'length), R1'length)); -- q * n
+             when QxN_COM=>  
+                 --R1 <= std_logic_vector(resize(unsigned(Q(2*s-1 downto s)) * to_unsigned(N, R1'length), R1'length)); -- q * n
                  R2<= std_logic_vector(resize(unsigned(C_shift) - unsigned(std_logic_vector(resize(unsigned(Q(2*s-1 downto k)) * to_unsigned(N, R1'length), R1'length))), R1'length)); -- r = c - q * n
-                 state <= R2_COM;
-                 --result<= std_logic_vector(resize(unsigned(result)+unsigned(C_shift) - unsigned(std_logic_vector(resize(unsigned(Q(2*s-1 downto s)) * to_unsigned(N, R1'length), R1'length))), R1'length));
-                 --result<= std_logic_vector(resize(unsigned(result)-to_unsigned(N, result'length)+unsigned(C_shift) - unsigned(std_logic_vector(resize(unsigned(Q(2*s-1 downto s)) * to_unsigned(N, R1'length), R1'length))), R1'length));
-                 --if unsigned(result) >= to_unsigned(N, result'length) then
-                 --   result<= std_logic_vector(resize(unsigned(result)-to_unsigned(N, result'length)+unsigned(C_shift) - unsigned(std_logic_vector(resize(unsigned(Q(2*s-1 downto s)) * to_unsigned(N, R1'length), R1'length))), R1'length));
-                 --else
-                 --   result<= std_logic_vector(resize(unsigned(result)+unsigned(C_shift) - unsigned(std_logic_vector(resize(unsigned(Q(2*s-1 downto s)) * to_unsigned(N, R1'length), R1'length))), R1'length));
-                 --end if;
-                when R2_COM=> 
-                 if unsigned(R2) >= to_unsigned(N, result'length) then                 
-                   result<= std_logic_vector(resize(unsigned(result)+unsigned(R2) - to_unsigned(N, R1'length) , R1'length));
+                 state <= AddR_COM;
+
+              when AddR_COM=> 
+                 if unsigned(R2) >= to_unsigned(N, T_reg'length) then                 
+                   T_reg<= std_logic_vector(resize(unsigned(T_reg)+unsigned(R2) - to_unsigned(N, R1'length) , R1'length));
                  else
-                   result<= std_logic_vector(resize(unsigned(result)+unsigned(R2), R1'length));
+                   T_reg<= std_logic_vector(resize(unsigned(T_reg)+unsigned(R2), R1'length));
                  end if;
-                  state <= R3_COM;
+                  state <= T_COM;
                 
                 
-                when R3_COM=> 
+               when T_COM=> 
                 
-                 if unsigned(result) >= to_unsigned(N, result'length) then                 
-                   result<= std_logic_vector(resize(unsigned(result) - to_unsigned(N, R1'length) , R1'length));
+                 if unsigned(T_reg) >= to_unsigned(N, T_reg'length) then                 
+                   T_reg<= std_logic_vector(resize(unsigned(T_reg) - to_unsigned(N, R1'length) , R1'length));
                  else
-                   result<= std_logic_vector(resize(unsigned(result), R1'length));
+                   T_reg<= std_logic_vector(resize(unsigned(T_reg), R1'length));
                  end if;
                  if j< l/w-1 then
                       j<=j+1;
@@ -183,5 +174,6 @@ begin
         end if;
     end process;
     fault<=fault_reg;
+    T<=T_reg;
 
 end Behavioral;
