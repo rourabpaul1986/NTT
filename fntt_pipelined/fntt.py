59fd419ae1e9ec5d4cb2a23a68ce0991d507a822
rourabpaul1986 <62745577+rourabpaul1986@users.noreply.github.com>
2025-04-22 12:23:27 +0530

    Add files via upload

---
+++ fntt_pipelined/fntt.py
@@ -0,0 +1,307 @@
+import math
+import random
+import numpy as np
+import ast
+import random
+#########################library to open nayuki website##############################
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+from datetime import datetime
+from selenium.common.exceptions import TimeoutException
+import time
+import os
+from datetime import datetime
+import webdriver_manager
+import selenium
+import sys
+import argparse
+import threading
+##################################################################################
+'''chrome_options = Options()
+#chrome_options.add_argument("--headless")  # Enable headless mode
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--window-size=1920x1080") 
+# Optionally, you can use ChromeDriverManager for automatic management of the driver
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+#driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+link="https://www.nayuki.io/page/number-theoretic-transform-integer-dft"
+driver.get(link)
+# Locate the input element by its ID and set the value
+input_element0 = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, "forward-transform-input-vector"))
+)
+
+input_element0.clear()  # Clear any existing value
+
+
+input_elementm = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, "forward-transform-minimum-working-modulus"))
+)
+input_elementm.clear()  # Clear any existing value
+
+input_elementw = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, "forward-transform-nth-root-of-unity"))
+)
+input_elementw.clear()  # Clear any existing value'''
+###########################################################################################
+def flip_random_bits(binary_str, n):
+    # Convert binary string to a list of characters for mutability
+    binary_list = list(binary_str)
+    
+    # Get the length of the binary number
+    length = len(binary_list)
+    
+    # Select n random positions in the binary string to flip
+    positions = random.sample(range(length), n)
+    
+    # Flip the bits at the chosen positions
+    for pos in positions:
+        # Flip the bit: if it's '0', change to '1', and vice versa
+        binary_list[pos] = '1' if binary_list[pos] == '0' else '0'
+    
+    # Convert the list back to a string and return it
+    return ''.join(binary_list)
+def rule_check(l, w, N):
+    if(l%w!=0):
+     print(f"Error: l={l} must be divisible by w={w}")
+     sys.exit(1)  # Exit with a non-zero status code indicating an error.
+    
+    n=math.ceil(math.log2(N))
+    #print(f"n={n} l={l}")
+    if(l<=n):
+     print(f"Error: l={l} must be grater than the number of binary bit required by N={N}")
+     sys.exit(1)  # Exit with a non-zero status code indicating an error.
+
+
+def l_binary(decimal, l):
+ binary = bin(decimal)[2:]
+ binary = binary.zfill(l) 
+ #print(f"{l} bit Binary representation of {decimal} is {binary}")
+ return binary
+
+def word_wise_montgomery_multiplication(A, B, N, N_prime, l, w, b, R, f, verbose):
+    T=0
+    Tf=0
+    m=l//w
+    fault=[0]*m
+    ####################################
+    unfault_A=A
+    fault_A = flip_random_bits(A, f)
+    #fault_A = A
+    ####################################
+    #xor_result = int(A,2) ^ int(fault_A,2)
+    #result_str = bin(xor_result)[2:].zfill(len(A))
+    #print(f"A: {A}, fault_A: {fault_A} no. of fault: {result_str} ")
+    
+    for i in range(m):
+       T0=T%b
+       B0=B[-w:]
+       #########################################
+       Aw=fault_A[-w:] 
+       At=unfault_A[-w:] 
+       k=random.randint(0, 10)
+       Awf=(int(At,2)+(k*b))%b
+       #########################################       
+       #print(f"Aw is {Aw} & B0 is {B0}")
+       u=((T0+int(Aw,2)*int(B0,2))*N_prime) % b
+       u_f=((T0+Awf*int(B0,2))*N_prime) % b
+       if(u!=u_f):
+        fault[i]=1
+       T=(T  +  (int(Aw,2)*int(B,2)) + (u*N)) // b 
+
+       Tf=(Tf  +  (Awf*int(B,2)) + (u*N)) // b 
+       #if verbose: 
+       # print(f"Aw: {int(Aw,2)%N}, Awf: {Awf}, T:{T}; Tf :{Tf}; T0: {T0}; u: {u} & u_f: {u_f} ")
+       
+       
+       fault_A = fault_A[:-w] 
+       unfault_A = unfault_A[:-w] 
+    # Step 4: Check if T >= N and perform final subtraction if necessary
+    if T >= N:
+     T = T-N
+    T=T*R % N
+    #if verbose:
+    # print(f"fault:{fault}")
+    return T
+
+
+def generate_random_numbers(n, a, b):
+    random_numbers = [random.randint(a, b) for _ in range(n)]
+    return random_numbers
+
+
+def mont_mult_top(l, w, A, B, N, f, verbose):
+
+    b=2**w 
+    N_prime=pow(-N,-1, b)
+    R=2**l
+
+    A_bin = l_binary(A, l)
+    B_bin = l_binary(B, l)
+    N_bin = l_binary(N, l)
+    #print(f"N_prime {N_prime}")
+    result= word_wise_montgomery_multiplication(A_bin, B_bin, N, N_prime, l, w, b, R, f, verbose)
+    #print(f"The result of Montgomery multiplication is: {result}")
+    return result
+
+def modmult(a, b, M):
+    return (a * b) % M
+
+def modpow(alpha, n, M):
+    if n == 0:
+        return 1
+    p = 1
+    bn = bin(n)
+    for b in bn[2:]:
+        p = modmult(p, p, M)
+        if b == '1':
+            p = modmult(p, alpha, M)
+    return p
+
+def is_primitive(alpha, N, M):
+    for i in range(1, N):
+        if modpow(alpha, i, M) == 1:
+            return False
+    return modpow(alpha, N, M) == 1
+
+def find_primitive(N, M):
+    for alpha in range(2, M):
+        if is_primitive(alpha, N, M):
+            return alpha
+    return 0
+
+def findinv(alpha, M):
+    for i in range(M):
+        if modmult(alpha, i, M) == 1:
+            return i
+
+def bit_reverse(x, n):
+    if type(x) == int:
+        b = '{:0{width}b}'.format(x, width=n)
+        return int(b[::-1], 2)
+    else:
+        x_out = np.zeros(len(x), dtype=int)
+        for i in range(len(x)):
+            x_out[i] = x[bit_reverse(i, n)]
+        return x_out
+
+def CT_Butterfly(i,j,n,x_out, A, twiddle_factor, M, length, f, l, w,  verbose):
+    #l=16
+    #w=4
+    #if verbose:print(f"t omega: {twiddle_factor}")
+    
+    if length <= 1:
+        x_out[0] = A[0]
+        return x_out
+    halflen = length >> 1
+    for k in range(halflen):
+        u = A[k]
+        #v = modmult(A[k + halflen], twiddle_factor, M) # this line should be on for school book multiplication
+        #if verbose: print (f"i:{i}, j:{j}, k:{k}, n:{n}, x_out{x_out}")
+        if verbose: print (f"i:{i}, j:{j}, k:{k}")
+        #print(f"twiddle_factor:{twiddle_factor}\t A[k + halflen]:{A[k + halflen]}")
+
+        
+        v=mont_mult_top(l, w, A[k + halflen], twiddle_factor, M, f, verbose=False)
+       
+        x_out[k] = (u + v) % M
+        x_out[k + halflen] = (u - v) % M
+    return x_out
+
+class fNTT:
+    def __init__(self, N, M, alpha=None):
+        self.N = N  # Degree of Polynomial 
+        self.M = M
+        self.Nlen = int(np.log2(N))
+        self.Ninv = findinv(self.N, self.M)
+        if alpha is not None and not is_primitive(alpha, N, M):
+            raise ValueError('Given alpha is not primitive')
+        self.alpha = alpha or find_primitive(N, M)
+        if self.alpha == 0:
+            raise ValueError('No primitive root exists')
+        print(f"initial omega: {self.alpha}")
+        self.alpha_modpow_table = [modpow(self.alpha, i, M) for i in range(N+1)]
+        self.bit_reverse_table = [bit_reverse(i, self.Nlen - 1) for i in range(N // 2)]
+        #print(f"self.bit_reverse_table :{self.bit_reverse_table} self.Nlen:{self.Nlen}")
+        #print(f"twidle factor :{self.alpha_modpow_table}")
+    def forward(self, x_in, f, l, w, verbose=False):
+        if len(x_in) != self.N:
+            raise ValueError(f'Input should be sized {self.N}')
+        x = np.copy(x_in)
+        
+        for i in range(self.Nlen):
+            n = 1 << i
+            seqlen = self.N // n
+            for j in range(n):
+                twiddle_factor = self.alpha_modpow_table[self.bit_reverse_table[j]]
+                if verbose:print(f"ct omega forward: {twiddle_factor} j:{j}")
+                CT_Butterfly(i,j,n,x[seqlen * j: seqlen * (j + 1)], x[seqlen * j: seqlen * (j + 1)], twiddle_factor, self.M, seqlen, f, l, w, verbose=False)
+        print("-------------------------------------") 
+        #print(f"\n \033[31mx: {x} (this array should be match with ram content)\033[0m")
+        print("\n\033[31mx: [" + ", ".join(f"{val:#x}" for val in x) + "] (this array should match with RAM content)\033[0m")
+
+        print("-------------------------------------")    
+        return bit_reverse([i % self.M for i in x], self.Nlen)
+
+    def inverse(self, x_in, f, l, w, verbose=False):
+        if len(x_in) != self.N:
+            raise ValueError(f'Input should be sized {self.N}')
+        x = np.copy(x_in)
+        
+        for i in range(self.Nlen):
+            n = 1 << i
+            seqlen = self.N // n
+            for j in range(n):
+                twiddle_factor = self.alpha_modpow_table[self.N - self.bit_reverse_table[j]]
+                CT_Butterfly(i,j,n,x[seqlen * j: seqlen * (j + 1)], x[seqlen * j: seqlen * (j + 1)], twiddle_factor, self.M, seqlen, f, l, w, verbose)
+        x = [modmult(i, self.Ninv, self.M) for i in x]
+        return bit_reverse(x, self.Nlen)
+def check_arrays(array1, array2):
+    if len(array1) != len(array2):
+        print(f"Wrong: Arrays have different lengths. {len(array1)} {len(array2)}")
+        return False
+    for i in range(len(array1)):
+        if array1[i] != array2[i]:
+            print(f"Wrong: Elements at index {i} are different ({array1[i]} != {array2[i]}).")
+            return False
+    print("Bingo !!  All elements match.")
+    return True
+if __name__ == "__main__":
+    verbose = '-v' in sys.argv
+    if '-v' in sys.argv:
+       print("Verbose mode activated")
+    #N = 16  # Size of the input sequence, must be a power of 2, degree of polynomial 
+    #M = 3329  # A prime number for modulus
+    parser = argparse.ArgumentParser(description="Process fault tolerance level and verbose mode.")
+    parser.add_argument("-n", "--degree", type=int, required=True, help="Fault tolerance level (integer value)")
+    parser.add_argument("-q", "--modulus", type=int, required=True, help="Sample Size (integer value)")   
+    parser.add_argument("--array", type=str, required=True, help="Comma-separated list of integers")
+    args = parser.parse_args()
+
+    # Accessing the arguments
+    N = args.degree
+    #n = 16  # Degree + 1, must be a power of 2
+    M = args.modulus
+    A = list(map(int, args.array.split(',')))
+    ntt = fNTT(N, M)
+    l = 12 #lenth of binary bits
+    w = 12  # how many binary bits will be processed
+    f=0 # number of fault bit
+    #A = generate_random_numbers(N, 0, M)
+    #A=[2121, 1613, 2930, 3245, 33, 812, 3142, 1735, 2264, 1857, 1339, 2566, 1549, 1783, 1794, 777]
+    print(f"Input Polynomial A of degree {len(A)-1}: {A}")
+    #print(f"Input Polynomial B of degree {len(B)-1}: {B}")
+    ntt_A = ntt.forward(A, f, l, w, verbose=verbose)
+    print(f"Forward NTT of A: {ntt_A}")
+    
+    inv_ntt_A = ntt.inverse(ntt_A,f, l, w, verbose=verbose)
+    print(f"Inverse NTT A: {inv_ntt_A}")
+    check_arrays(inv_ntt_A, A)
---
+++ fntt_pipelined/mem_gen.py
@@ -0,0 +1,209 @@
+import math
+import random
+import numpy as np
+import ast
+import argparse
+import subprocess
+import os
+
+
+def generate_variant_pkg(n, q, filename="./src/variant_pkg.vhd"):
+    with open(filename, "w") as f:
+        f.write(f"""library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use ieee.math_real.all;
+
+package variant_pkg is
+
+constant n  : integer := {n};
+constant q  : integer := {q};
+constant logn : positive := positive(ceil(log2(real(n))));
+constant lognby2 : positive := positive(ceil(log2(real(n/2))));
+constant logq : positive := positive(ceil(log2(real(q))));
+
+end variant_pkg;
+""")
+
+
+
+
+
+def generate_poly_mem_vhd(A, filename="./src/poly_mem.vhd"):
+    with open(filename, "w") as f:
+        f.write("""----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: {date}
+-- Design Name: 
+-- Module Name: poly_mem - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use work.variant_pkg.all;
+use IEEE.NUMERIC_STD.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity poly_mem is
+    port(   clk : in std_logic; --clock
+            wr_en : in std_logic;   --write enable for port 0
+            data_in0 : in std_logic_vector(logq-1 downto 0);   --Input data to port 0.
+            data_in1 : in std_logic_vector(logq-1 downto 0);   --Input data to port 0.
+            addr_rd_0 : in std_logic_vector(logn-1 downto 0);    --address for port 0
+            addr_rd_1 : in std_logic_vector(logn-1 downto 0);    --address for port 1
+            addr_wr_0 : in std_logic_vector(logn-1 downto 0);    --address for port 0
+            addr_wr_1 : in std_logic_vector(logn-1 downto 0);    --address for port 1
+            ce : in std_logic;   --enable port 0.
+            rd_en : in std_logic;   --enable port 1.
+            data_out_0 : out std_logic_vector(logq-1 downto 0);  --output data from port 0.
+            data_out_1 : out std_logic_vector(logq-1 downto 0)   --output data from port 1.
+        );
+end poly_mem;
+
+architecture Behavioral of poly_mem is
+
+    type ram_type is array(0 to {max_index}) of std_logic_vector(logq-1 downto 0);
+    signal ram : ram_type := (
+""".format(date="04/08/2025", max_index=len(A)-1))
+
+        for i, val in enumerate(A):
+            comma = ',' if i < len(A) - 1 else ''
+            f.write(f"        {i:>2} => std_logic_vector(to_unsigned({val}, logq)){comma}\n")
+
+        f.write("""    );
+
+begin
+    process(clk)
+    begin
+        if(rising_edge(clk)) then
+            --For port 0. Writing.
+            if(ce = '1') then    --see if write enable is ON.
+                if(wr_en = '1') then    --see if write enable is ON.
+                    ram(conv_integer(addr_wr_0)) <= data_in0;
+                    ram(conv_integer(addr_wr_1)) <= data_in1;
+                end if;
+            end if;
+        end if;
+    end process;
+    
+    process(clk)
+    begin
+    if(rising_edge(clk)) then
+            --For port 0. Writing.
+            if(ce = '1') then    --see if write enable is ON.
+                if(rd_en = '1') then    --see if write enable is ON.
+                    data_out_0 <= ram(conv_integer(addr_rd_0));
+                    data_out_1 <= ram(conv_integer(addr_rd_1));
+                end if;
+            end if;
+        end if;
+    end process;
+
+
+end Behavioral;
+""")
+
+
+
+
+
+def generate_random_numbers(n, a, b):
+    return [random.randint(a, b) for _ in range(n)]
+    
+    
+def generate_twidle(N, q, alpha=None):
+    Nlen = int(np.log2(N))
+    Ninv = findinv(N, q)
+    if alpha is not None and not is_primitive(alpha, N, q):
+        raise ValueError('Given alpha is not primitive')
+    alpha = alpha or find_primitive(N, q)
+    if alpha == 0:
+        raise ValueError('No primitive root exists')
+    print(f"initial omega: {alpha}")
+    w = [modpow(alpha, i, q) for i in range(N+1)]
+    return w
+    
+    
+def findinv(alpha, M):
+    for i in range(M):
+        if modmult(alpha, i, M) == 1:
+            return i
+def modmult(a, b, M):
+    return (a * b) % M
+
+def modpow(alpha, n, M):
+    if n == 0:
+        return 1
+    p = 1
+    bn = bin(n)
+    for b in bn[2:]:
+        p = modmult(p, p, M)
+        if b == '1':
+            p = modmult(p, alpha, M)
+    return p
+    
+def is_primitive(alpha, N, M):
+    for i in range(1, N):
+        if modpow(alpha, i, M) == 1:
+            return False
+    return modpow(alpha, N, M) == 1
+
+def find_primitive(N, M):
+    for alpha in range(2, M):
+        if is_primitive(alpha, N, M):
+            return alpha
+    return 0
+parser = argparse.ArgumentParser(description="Process fault tolerance level and verbose mode.")
+parser.add_argument("-n", "--degree", type=int, required=True, help="Fault tolerance level (integer value)")
+parser.add_argument("-q", "--modulus", type=int, required=True, help="Sample Size (integer value)")
+args = parser.parse_args()
+
+# Accessing the arguments
+n = args.degree
+
+q = args.modulus
+
+
+# Generate polynomials
+A = generate_random_numbers(n, 0, q)
+#A = [2121, 1613, 2930, 3245, 33, 812, 3142, 1735, 2264, 1857, 1339, 2566, 1549, 1783, 1794, 777]
+#B = [2121, 1613, 2930, 3245, 33, 812, 3142, 1735, 2264, 1857, 1339, 2566, 1549, 1783, 1794, 777]
+w = generate_twidle(n, q)
+generate_poly_mem_vhd(A)
+generate_variant_pkg(n, q)
+# Print polynomials
+print(f"Input Polynomial A of degree {len(A)-1}: {A}")
+#print(f"Input Polynomial B of degree {len(B)-1}: {B}")
+print(f"Twiddle factor (w) : {w}")
+A_str = ",".join(map(str, A))  # Serialize list
+subprocess.run([
+    "python3", "fntt.py",
+    "-n", str(n),
+    "-q", str(q),
+    "--array", A_str
+])
+# Write to COE file
+with open("w_mem.coe", "w") as f:
+    #f.write("#Xilinx COE file for polynomials B\n")
+    f.write("memory_initialization_radix=10;\n")
+    f.write("memory_initialization_vector=\n")
+    
+    # Write w coefficients
+    #f.write("# Polynomial B coefficients\n")
+    f.write(", ".join(map(str, w)) + ";\n")
+
+print("COE file 'w_mem.coe' generated successfully.")
