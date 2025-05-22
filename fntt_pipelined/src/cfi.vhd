d06b9c656b7de8ef9a595a4c0e6305e7a6b51a6f
rourabpaul1986 <62745577+rourabpaul1986@users.noreply.github.com>
2025-05-22 14:49:35 +0530

    removed seprate cfi fault signals form cfi.vhd

--- fntt_pipelined/src/cfi.vhd
+++ fntt_pipelined/src/cfi.vhd
@@ -15,18 +15,19 @@ entity cfi is
         wr_en         : in  std_logic;
         poly_mem_ce  : in  std_logic;
         uv_rst    : in  std_logic;
-        barrett_cfi_fault  : out  std_logic;
-        mem_cfi_fault  : out  std_logic;
-        uv_cfi_fault  : out  std_logic
+        cfi_fault  : out  std_logic
     );
 end cfi;
 
 architecture Behavioral of cfi is
     signal shift_reg : std_logic_vector(3 downto 0) := (others => '0');
     signal fault_cwr : std_logic_vector(3 downto 0) := (others => '0');
-    signal loop_counter   : integer range 0 to max_loop := 0;
+    --signal loop_counter   : integer range 0 to max_loop := 0;
     signal uv_wr    : std_logic := '0';
     signal barrett_ctrl    : std_logic := '0';
+    signal barrett_cfi_fault  :  std_logic;
+    signal uv_cfi_fault  :  std_logic;
+    signal mem_cfi_fault  :  std_logic;
 begin
     uv_wr<= wr_en and (not uv_rst);
     
@@ -41,7 +42,7 @@ begin
                      
 --    mem_cfi_fault <= '0' when fault_cwr(3)=shift_reg(3) and fault_cwr(0)=shift_reg(0) and poly_mem_ce=barrett_ctrl and loop_counter <max_loop+1 else
 --                     '1';
-     mem_cfi_fault <= '0' when fault_cwr(3)=shift_reg(3) and fault_cwr(0)=shift_reg(0) and loop_counter <max_loop+1 else
+    mem_cfi_fault <= '0' when fault_cwr(3)=shift_reg(3) and fault_cwr(0)=shift_reg(0) else-- and loop_counter <max_loop+1 else
                      '1';                 
     uv_cfi_fault <= '0' when fault_cwr(0)=shift_reg(0) else
                      '1';
@@ -51,15 +52,15 @@ begin
                 fault_cwr<= rd_en & wr_t2 & wr_t3 & uv_wr;
                 if rd_en = '0' then
                      shift_reg <= '0' & shift_reg(3 downto 1);  -- Right shift with '0' into MSB
-                     loop_counter<=0;
+                     --loop_counter<=0;
                  elsif rd_en = '1' then
                     shift_reg <= '1' & shift_reg(3 downto 1);  -- Right shift with '1' into MSB
-                    loop_counter <= loop_counter + 1;
+                    --loop_counter <= loop_counter + 1;
                 end if;
             end if;
         --end if;
     end process;
 
-  
+  cfi_fault<=barrett_cfi_fault and uv_cfi_fault and mem_cfi_fault;
 
 end Behavioral;
