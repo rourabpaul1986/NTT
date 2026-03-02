d3ad7e37f6e986d22fddedabade0a731f1e0dfc1
rourabpaul1986 <62745577+rourabpaul1986@users.noreply.github.com>
2026-03-02 12:39:32 +0530

    Update variant_pkg.vhd

--- barrett/pipelined_word_wise/variant_pkg.vhd
+++ barrett/pipelined_word_wise/variant_pkg.vhd
@@ -1,15 +1,22 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
-use ieee.math_real.all;
+use IEEE.NUMERIC_STD.ALL;
 
 package variant_pkg is
 
-constant l  : integer := 12;
-constant q  : integer := 3329;
+constant l  : integer := 32;
+--constant q : integer := 3329; --kyber --constant q : integer := 8380417;--Dilithium --
+--constant q : integer := 12289;--falcon, ntru
+--constant q_u : unsigned(l-1 downto 0) := x"100400001230002D";
+--constant q_u : unsigned(l-1 downto 0) := x"D01";  --kyber
+--constant q_u : unsigned(l-1 downto 0) := x"3001"; --12289;--falcon, ntru
+--constant q_u : unsigned(l-1 downto 0) := x"3FFFD001";
+constant q_u : unsigned(l-1 downto 0) := x"6C000001"; --CKSS
+constant q : integer := to_integer(q_u);
 
-
-constant logq : positive := 12;
-constant w : positive := 4;
+constant logq : positive := l;
+constant w : positive := 8;
+constant k : integer := 2*logq; --32 for w=4
+--constant mu : integer := (2**(2*logq)) / q; --2^k//n
+constant mu_vec : unsigned(k-1 downto 0) := to_unsigned((2**(2*logq)) / q, k);
 end variant_pkg;
